a4053202bb3db8527e24ed2861ea476af89875de
wcb <wcb@sloong.com>
2021-09-23 20:55:22 +0800

    refactor: convert to hpp format. remove the cpp file.

--- lib/linux_cpuload.hpp
+++ lib/linux_cpuload.hpp
@@ -14,8 +14,25 @@
 #include <map>
 #include <unordered_map>
 #include <chrono>
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <sstream>
 
-class cpuLoad {
+const std::vector<std::string> cpuIdentifiers{"user",
+                                              "nice",
+                                              "system",
+                                              "idle",
+                                              "iowait",
+                                              "irq",
+                                              "softirq",
+                                              "steal",
+                                              "guest",
+                                              "guest_nice"};
+
+class cpuLoad
+{
 
 public:
     cpuLoad() = delete;
@@ -23,24 +40,35 @@ public:
      * @brief constructor
      * @param procFileName
      */
-    explicit cpuLoad(std::string procFileName = "/proc/stat"):
-        procFile(std::move(procFileName)), cpuName("") {};
+    explicit cpuLoad(std::string procFileName = "/proc/stat") : procFile(std::move(procFileName)), cpuName(""){};
 
     /**
      * @brief initialize the parsing algo
      */
-    void initCpuUsage();
+    void initCpuUsage()
+    {
+
+        this->parseStatFile(this->procFile);
+        this->calculateCpuUsage();
+        this->currentTime = std::chrono::system_clock::now() - std::chrono::milliseconds(2000);
+    }
     /**
      * @brief get current cpu load in 0-100%
      * @return current cpu load
      */
-    double getCurrentCpuUsage();
+    double getCurrentCpuUsage()
+    {
+
+        this->upDateCPUUsage();
+        return this->cpuUsage.at("cpu");
+    }
 
     /**
      * @brief get Cpu user / nice / system /idle time. used for cpu usage per process
      * @return tuple<user,nice,system,idle>
      */
-    std::tuple<uint64_t , uint64_t, uint64_t, uint64_t> getCpuTimes() {
+    std::tuple<uint64_t, uint64_t, uint64_t, uint64_t> getCpuTimes()
+    {
         auto cpuLoad_ = this->parseStatFile(this->procFile);
         return std::make_tuple(cpuLoad_.at("cpu").at("user"),
                                cpuLoad_.at("cpu").at("nice"),
@@ -52,18 +80,149 @@ public:
      * @brief get cpu Usage of all cores in percent
      * @return vector of cpu load per core 0-100%
      */
-    std::vector<double> getCurrentMultiCoreUsage();
+    std::vector<double> getCurrentMultiCoreUsage()
+    {
+        this->upDateCPUUsage();
+        std::vector<double> percents;
+        for (const auto &elem : this->cpuUsage)
+        {
+            if (elem.first == "cpu")
+            {
+                continue;
+            }
+            percents.push_back(elem.second);
+        }
+        return percents;
+    }
     /**
      * @brief get CPU Description
      * @param cpuNameFile - typical /proc/cpuinfo
      * @return cpu Type string
      */
-    std::string getCPUName(const std::string& cpuNameFile = "/proc/cpuinfo");
+    std::string getCPUName(const std::string &cpuNameFile = "/proc/cpuinfo")
+    {
+
+        if (!this->cpuName.empty())
+        {
+            return this->cpuName;
+        }
+
+        std::ifstream file;
+        file.open(cpuNameFile);
+
+        if (!file.is_open())
+        {
+            throw std::runtime_error("unable to open " + cpuNameFile);
+        }
+        std::string line;
+        while (std::getline(file, line))
+        {
+            if (line.find("model name") != std::string::npos)
+            {
+                size_t pos = line.find(':');
+                if (pos != std::string::npos)
+                {
+                    this->cpuName = line.substr(pos, line.size());
+                    return this->cpuName;
+                }
+            }
+        }
+        return std::string();
+    }
 
 private:
-    void calculateCpuUsage();
-    std::map<std::string, std::unordered_map<std::string, uint64_t>> parseStatFile(const std::string& fileName);
-    void upDateCPUUsage();
+    void calculateCpuUsage()
+    {
+        for (const auto &elem : this->cpuLoadMap)
+        {
+
+            if (this->cpuLoadMap.at(elem.first).at("user") < this->oldCpuLoadMap.at(elem.first).at("user") ||
+                this->cpuLoadMap.at(elem.first).at("nice") < this->oldCpuLoadMap.at(elem.first).at("nice") ||
+                this->cpuLoadMap.at(elem.first).at("system") < this->oldCpuLoadMap.at(elem.first).at("system") ||
+                this->cpuLoadMap.at(elem.first).at("idle") < this->oldCpuLoadMap.at(elem.first).at("idle"))
+            {
+            }
+            else
+            {
+                auto total = (this->cpuLoadMap.at(elem.first).at("user") - this->oldCpuLoadMap.at(elem.first).at("user")) +
+                             (this->cpuLoadMap.at(elem.first).at("nice") - this->oldCpuLoadMap.at(elem.first).at("nice")) +
+                             (this->cpuLoadMap.at(elem.first).at("system") -
+                              this->oldCpuLoadMap.at(elem.first).at("system"));
+
+                double percent = total;
+                total += (this->cpuLoadMap.at(elem.first).at("idle") - this->oldCpuLoadMap.at(elem.first).at("idle"));
+                percent /= total;
+                percent *= 100.0;
+                this->cpuUsage[elem.first] = percent;
+            }
+        }
+    }
+    std::map<std::string, std::unordered_map<std::string, uint64_t>> parseStatFile(const std::string &fileName)
+    {
+
+        std::map<std::string, std::unordered_map<std::string, uint64_t>> cpuLoad_;
+
+        try
+        {
+            std::ifstream cpuFile(fileName);
+
+            uint32_t lineCnt = 0;
+            bool infoValid = true;
+            for (std::string line; std::getline(cpuFile, line) && infoValid; lineCnt++)
+            {
+
+                std::stringstream strStream(line);
+                std::string strPart;
+                std::string cpuNum;
+                auto it = cpuIdentifiers.begin();
+                std::unordered_map<std::string, uint64_t> cpuValues;
+                while (strStream >> strPart)
+                {
+                    if (cpuNum.empty())
+                    {
+                        if (strPart.find("cpu") != std::string::npos)
+                        {
+                            cpuNum = strPart;
+                            continue;
+                        }
+                        else
+                        {
+                            infoValid = false;
+                            break;
+                        }
+                    }
+                    if (it != cpuIdentifiers.end())
+                    {
+                        cpuValues[it->data()] = std::stoull(strPart);
+                    }
+                    if (it->data() == cpuIdentifiers.at(4))
+                    {
+                        break;
+                    }
+                    it++;
+                }
+                if (!cpuNum.empty())
+                {
+                    cpuLoad_[cpuNum] = cpuValues;
+                }
+            }
+        }
+        catch (std::ifstream::failure &e)
+        {
+            throw std::runtime_error("Exception: " + fileName + std::string(e.what()));
+        }
+        return cpuLoad_;
+    }
+    void upDateCPUUsage()
+    {
+        if (!((this->currentTime + std::chrono::milliseconds(1000)) > std::chrono::system_clock::now()))
+        {
+            this->oldCpuLoadMap = this->cpuLoadMap;
+            this->currentTime = std::chrono::system_clock::now();
+            this->cpuLoadMap = this->parseStatFile(this->procFile);
+            this->calculateCpuUsage();
+        }
+    }
     std::chrono::system_clock::time_point currentTime;
     std::string procFile;
     std::string cpuName;
@@ -71,5 +230,3 @@ private:
     std::map<std::string, std::unordered_map<std::string, uint64_t>> cpuLoadMap;
     std::map<std::string, std::unordered_map<std::string, uint64_t>> oldCpuLoadMap;
 };
-
-
--- lib/linux_memoryload.hpp
+++ lib/linux_memoryload.hpp
@@ -1,3 +1,12 @@
+/*** 
+ * @Author: Chuanbin Wang - wcb@sloong.com
+ * @Date: 2021-09-23 17:27:14
+ * @LastEditTime: 2021-09-23 17:28:16
+ * @LastEditors: Chuanbin Wang
+ * @FilePath: /Linux-System-Monitoring-Library/lib/linux_memoryload.hpp
+ * @Copyright 2015-2020 Sloong.com. All Rights Reserved
+ * @Description: 
+ */
 /**
  * @author: Daniel Fuchs
  * @contact: fuxeysolutions@gmail.com
@@ -8,6 +17,10 @@
  */
 #pragma once
 
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <cinttypes>
 #include <string>
 #include <chrono>
 
@@ -26,32 +39,74 @@ public:
      * @brief get total memory of the system in KB
      * @return total memory in KB
      */
-    uint64_t getTotalMemoryInKB();
+    uint64_t getTotalMemoryInKB(){
+    this->parseMemoryFile();
+    return this->totalMemoryInKB;
+}
     /**
      * @brief get current Memory Usage of the system in KB
      * @return used memory in KB
      */
-    uint64_t getCurrentMemUsageInKB();
+    uint64_t getCurrentMemUsageInKB(){
+    this->parseMemoryFile();
+    return this->getTotalMemoryInKB() - this->currentMemoryUsageInKB;
+}
+
     /**
      * @brief get current memory usage of the system in percent 0-100%
      * @return 0-100%
      */
-    float getCurrentMemUsageInPercent();
+    float getCurrentMemUsageInPercent(){
+    this->parseMemoryFile();
+    uint64_t memavail = this->getCurrentMemUsageInKB();
+    return round((((memavail * 100.0 / this->getTotalMemoryInKB()))) * 100.0) / 100.0;
+}
     /**
      * @brief get the current memory usage of a process
      * @param pid  - process id
      * @return memory usage in KB
      */
-    static uint64_t getMemoryUsedByProcess(int pid);
+    static uint64_t getMemoryUsedByProcess(int pid) {
+    return memoryLoad::parseProcessMemoryFile("/proc/" + std::to_string(pid) + "/status");
+}
     /**
      * @brief get memory usage of this process (self)
      * @return memory usage in KB
      */
-    uint64_t getMemoryUsageByThisProcess();
+    uint64_t getMemoryUsageByThisProcess() {
+    return this->parseProcessMemoryFile(this->memInfoOfProcessFile);
+}
 
 private:
-    bool parseMemoryFile();
-    static uint64_t parseProcessMemoryFile(std::string fileToParse);
+    bool parseMemoryFile(){
+    if(timeStamp + std::chrono::milliseconds(100) > std::chrono::steady_clock::now()) {
+        return true;
+    }
+    std::ifstream memoryFile;
+    memoryFile.open(this->memInfoFile);
+    this->timeStamp = std::chrono::steady_clock::now();
+    if (!memoryFile.is_open()) {
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(memoryFile, line)) {
+        sscanf(line.c_str(), "MemTotal: %" PRIu64, &this->totalMemoryInKB);
+        sscanf(line.c_str(), "MemAvailable: %" PRIu64, &this->currentMemoryUsageInKB);
+    }
+    memoryFile.close();
+    return true;
+}
+    static uint64_t parseProcessMemoryFile(std::string fileToParse){
+    uint64_t MemFree = 0;
+    std::ifstream memoryFile;
+    memoryFile.open(fileToParse);
+    std::string line;
+    while (std::getline(memoryFile, line)) {
+        sscanf(line.c_str(), "VmSize: %" PRIu64, &MemFree);
+    }
+    return MemFree;
+}
     uint64_t totalMemoryInKB;
     uint64_t currentMemoryUsageInKB;
     std::string memInfoFile;
